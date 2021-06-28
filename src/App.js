0c3410a470a7bcc94b4e0780988dd77532ea190d
nvdinh185 <nvdinh0766777123@gmail.com>
2021-06-28 15:35:03 +0700

    not ok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import { Component } from 'react'
-import Table from './react/Table'
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <Table />
-            </div>
-        )
-    }
-}
+const App = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    return (
+        <>
+            <Button type="primary" onClick={showModal}>
+                Open Modal
+            </Button>
+            <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
+        </>
+    );
+};
 
 export default App
